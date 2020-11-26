2cea179f9f4184a7d3d510df47d93cc5d212fb25
omerfarukcengizzzz <omerfarukcengizzzz@gmail.com>
2020-11-26 23:30:47 +0100

    Ticketing Project - MVC

--- src/main/java/com/cybertek/controller/UserController.java
+++ src/main/java/com/cybertek/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +22,19 @@ public class UserController {
 
     @GetMapping({"/create", "/add", "/initialize"})
     public String createUser(Model model) {
+        model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+
+        return "/user/create";
+    }
+
+    @PostMapping("/create")
+    public String insertUser(UserDTO user, Model model) {
+
+        userService.save(user);
+
+        // we need; user, roles, users
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
         model.addAttribute("users", userService.findAll());
